36f349d9546f8f0b6e6036a1103142d829e98592
您的姓名 <Email>
2022-09-03 21:28:32 +0800

    Update 2022-09-03T13:28:29.402Z

---
+++ assets/js/all.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5');
+});
+$(function () {
+  $(".reply").click(function (e) {
+    event.preventDefault();
+    $(".card-editor").toggleClass("text-hide");
+    $(".andy-card").toggleClass("text-hide");
+  });
+});
+//# sourceMappingURL=all.js.map
